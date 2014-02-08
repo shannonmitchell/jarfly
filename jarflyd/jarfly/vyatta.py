7b561ea1aeb5b15f4d98933127ff0f26f009bfa8
root <root@wiki.linuxrackers.com>
2014-02-07 19:38:06 -0600

    Cleaned up some formatting and added some logging.

--- jarflyd/jarfly/vyatta.py
+++ jarflyd/jarfly/vyatta.py
@@ -16,8 +16,12 @@ def configureVyattaLogin(vserver, vpass, keytype, keyval, keyid):
     client.connect(vserver, username='vyatta', password=vpass)
     commands = """
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set system login user vyatta authentication public-keys """ + keyid + """ key """ + keyval + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set system login user vyatta authentication public-keys """ + keyid + """ type """ + keytype + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set system login user vyatta authentication public-keys """ \
+            + keyid + """ key """ + keyval + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set system login user vyatta authentication public-keys """ \
+            + keyid + """ type """ + keytype + """
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
@@ -25,7 +29,7 @@ def configureVyattaLogin(vserver, vpass, keytype, keyval, keyid):
     jarlog.logit('INFO', "Running initial viatta login config")
     stdin, stdout, stderr = client.exec_command(commands)
     for line in stdout:
-        print '... ' + line.strip('\n')
+        jarlog.logit('INFO', '... ' + line.strip('\n'))
 
     client.close()
 
@@ -38,15 +42,24 @@ def configureVyattaNats(vserver, vpass, dmznet, appnet, datanet):
     client.connect(vserver, username='vyatta', password=vpass)
     commands = """
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 10 source address """ + dmznet + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 10 outbound-interface eth0
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 10 translation address masquerade
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 20 source address """ + appnet + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 20 outbound-interface eth0
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 20 translation address masquerade
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 30 source address """ + datanet + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 30 outbound-interface eth0
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set nat source rule 30 translation address masquerade
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 10 source address """ + dmznet + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 10 outbound-interface eth0
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 10 translation address masquerade
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 20 source address """ + appnet + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 20 outbound-interface eth0
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 20 translation address masquerade
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 30 source address """ + datanet + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 30 outbound-interface eth0
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set nat source rule 30 translation address masquerade
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
@@ -54,13 +67,13 @@ def configureVyattaNats(vserver, vpass, dmznet, appnet, datanet):
     jarlog.logit('INFO', "Running initial viatta app config" + commands)
     stdin, stdout, stderr = client.exec_command(commands)
     for line in stdout:
-        print '... ' + line.strip('\n')
+        jarlog.logit('INFO', '... ' + line.strip('\n'))
 
     client.close()
 
 
-
-def configureVyattaVPN(vserver, vpass, vpnshared_pass, vpnuser, vpnpass, range_start, range_end):
+def configureVyattaVPN(vserver, vpass, vpnshared_pass,
+                       vpnuser, vpnpass, range_start, range_end):
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -68,16 +81,30 @@ def configureVyattaVPN(vserver, vpass, vpnshared_pass, vpnuser, vpnpass, range_s
     client.connect(vserver, username='vyatta', password=vpass)
     commands = """
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec ipsec-interfaces interface eth0
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn ipsec ipsec-interfaces interface eth0
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec nat-traversal enable
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn ipsec nat-networks allowed-network 0.0.0.0/0
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access outside-address """ + vserver + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access client-ip-pool start """ + range_start + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access client-ip-pool stop """ + range_end + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access ipsec-settings authentication mode pre-shared-secret
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access ipsec-settings authentication pre-shared-secret """ + vpnshared_pass + """
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access authentication mode local
-    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set vpn l2tp remote-access authentication local-users username """ + vpnuser + """ password """ + vpnpass + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn ipsec nat-networks allowed-network 0.0.0.0/0
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access outside-address """ + vserver + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access client-ip-pool start """ \
+            + range_start + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access client-ip-pool stop """ \
+            + range_end + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access ipsec-settings authentication \
+            mode pre-shared-secret
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access ipsec-settings authentication \
+            pre-shared-secret """ + vpnshared_pass + """
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access authentication mode local
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper \
+            set vpn l2tp remote-access authentication local-users \
+            username """ + vpnuser + """ password """ + vpnpass + """
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
     /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
@@ -85,9 +112,10 @@ def configureVyattaVPN(vserver, vpass, vpnshared_pass, vpnuser, vpnpass, range_s
     jarlog.logit('INFO', "Running initial viatta app config" + commands)
     stdin, stdout, stderr = client.exec_command(commands)
     for line in stdout:
-        print '... ' + line.strip('\n')
+        jarlog.logit('INFO', '... ' + line.strip('\n'))
 
     client.close()
+
 
 def configureDevice(confobj, curjar, dmznet, appnet, datanet, keyname):
 
@@ -144,45 +172,48 @@ def configureDevice(confobj, curjar, dmznet, appnet, datanet, keyname):
             curserver = server
 
     if addserver == 1:
+
+        # Create the vyatta device
         origserver = csobj.servers.create(vyatta_name, vimage.id, vflavor.id,
                                           key_name=keyname, nics=nics_list)
 
+        # Wait for it to finish
         curserver = pyrax.utils.wait_until(origserver, "status",
                                            ["ACTIVE", "ERROR"])
         print "Server Password "
         print curserver.adminPass
 
-    # Add domain entry for the vyatta device
-    jardns.addRecord(vyatta_name, curserver.accessIPv4)
+        # Add domain entry for the vyatta device
+        jardns.addRecord(vyatta_name, curserver.accessIPv4)
 
-    # print the network & pass info
-    print "Server Networks: "
-    print curserver.accessIPv4
+        # print the network & pass info
+        print "Server Networks: "
+        print curserver.accessIPv4
 
-    # Sleep for a few seconds to wait for things to process before
-    # configuration
-    time.sleep(10)
+        # Sleep for a few seconds to wait for things to process before
+        # configuration
+        time.sleep(10)
 
-    # Configure the Vyatta Login
-    configureVyattaLogin(curserver.accessIPv4, curserver.adminPass,
-                         keytype, keyval, keyid)
+        # Configure the Vyatta Login
+        configureVyattaLogin(curserver.accessIPv4, curserver.adminPass,
+                             keytype, keyval, keyid)
 
-    # Configure the Vyatta Networking
-    dmznet = confobj.get(curjar, "dmznet_cidr")
-    appnet = confobj.get(curjar, "appnet_cidr")
-    datanet = confobj.get(curjar, "datanet_cidr")
-    configureVyattaNats(curserver.accessIPv4, curserver.adminPass,
-                        dmznet, appnet, datanet)
+        # Configure the Vyatta Networking
+        dmznet = confobj.get(curjar, "dmznet_cidr")
+        appnet = confobj.get(curjar, "appnet_cidr")
+        datanet = confobj.get(curjar, "datanet_cidr")
+        configureVyattaNats(curserver.accessIPv4, curserver.adminPass,
+                            dmznet, appnet, datanet)
 
-    # Configure a Vyatta VPN
-    vpn_shared_pass = confobj.get(curjar, "vpn_shared_pass")
-    vpn_username = confobj.get(curjar, "vpn_username")
-    vpn_password = confobj.get(curjar, "vpn_password")
-    vpn_client_ip_range_start = confobj.get(curjar, "vpn_client_ip_range_start")
-    vpn_client_ip_range_end = confobj.get(curjar, "vpn_client_ip_range_end")
-    configureVyattaVPN(curserver.accessIPv4, curserver.adminPass,
-                       vpn_shared_pass, vpn_username, vpn_password,
-                       vpn_client_ip_range_start,
-                       vpn_client_ip_range_end)
-
-
+        # Configure a Vyatta VPN
+        vpn_shared_pass = confobj.get(curjar, "vpn_shared_pass")
+        vpn_username = confobj.get(curjar, "vpn_username")
+        vpn_password = confobj.get(curjar, "vpn_password")
+        vpn_client_ip_range_start = confobj.get(curjar,
+                                                "vpn_client_ip_range_start")
+        vpn_client_ip_range_end = confobj.get(curjar,
+                                              "vpn_client_ip_range_end")
+        configureVyattaVPN(curserver.accessIPv4, curserver.adminPass,
+                           vpn_shared_pass, vpn_username, vpn_password,
+                           vpn_client_ip_range_start,
+                           vpn_client_ip_range_end)
